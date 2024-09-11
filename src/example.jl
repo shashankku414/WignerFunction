9b4cc5b236fe8223e8002b14359021867d614c76
shashankku414 <shashank11297@gmail.com>
2024-09-11 16:33:05 -0400

    initial

---
+++ src/example.jl
@@ -0,0 +1,26 @@
+using DelimitedFiles
+include("./tomo.jl")
+using .tomo
+
+
+load_dir = "../example_data" # Specify the location of the experimetal data
+time_delay = readdlm("$(load_dir)/timedelay.dat") #import the time delay array from same
+work_dir = "../work" # Specify the location where do you want to save the computed data
+println("Number of available threads = $(Threads.nthreads())") # For parallel computing > 1
+Threads.@threads for i in eachindex(time_delay)
+    save_dir = "$(work_dir)/$(i-1)"
+    run(`mkdir -p $(save_dir)`) # Make a different directory for each time delay
+    x_range, phi_range, pr_data = dataload("$(load_dir)/$(i-1)", (-1000, 1000), 1000) #Calculate the PDF from data
+    writedlm("$(save_dir)/i-.dat", collect(x_range))
+    writedlm("$(save_dir)/phi.dat", collect(phi_range))
+    writedlm("$(save_dir)/pr.dat", pr_data) #Save the PDF data to plot histograms
+    # Calculate the field quadratures and the Wigner function values
+    # q1,q2 are the lower and higher limits of Quadrature Q
+    # p1,p2 are the lower and higher limits of Quadrature P
+    # density will specify the number of grid points
+    # kc is the cut off limit for filter Kernal (Do not change)
+    Q, P, W = tomography(pr_data, collect(phi_range), collect(x_range); q1=-5, q2=5, p1=-5, p2=5, density=100, kc=5)
+    writedlm("$(save_dir)/Q.dat", Q)
+    writedlm("$(save_dir)/P.dat", P)
+    writedlm("$(save_dir)/W.dat", W) #Save the values for Wigner plots
+end
---
+++ src/tomo.jl
@@ -0,0 +1,78 @@
+module tomo
+
+using JSON
+# using Interpolations
+using Trapz
+using Plots
+using Base.Threads
+using DelimitedFiles
+using KernelDensity
+
+export tomography, dataload
+
+function dataload(filename :: String, kde_lims :: Tuple, num_points :: Int64)
+    data_array = readdlm(filename)
+    phi_data = range(0, 2*pi, length=size(data_array, 2))
+    pr = zeros(size(data_array, 2), num_points)
+    # for i in 1:size(data_array, 2)
+    for i in axes(data_array, 2)
+        y = kde(data_array[:, i], boundary=kde_lims, npoints=num_points)
+        pr[i, :] = y.density
+    end
+    return range(kde_lims[1], kde_lims[2], length=num_points), phi_data, pr
+end
+
+function K(arg, kc)
+    return ((kc^2)/2.)*(1-((kc^2)*(arg^2)/4.)+((kc^4)*(arg^4)/72.)-((kc^6)*(arg^6)/2880.)+((kc^8)*(arg^8)/201600.))
+end
+
+function Kor(arg, kc)
+    return (cos(kc*arg) + kc*arg*sin(kc*arg) - 1)/(arg^2)
+end
+
+function Kcomp(q, p, angle, volt, kc)
+    turn = 0.01
+    arg = (q*cos(angle)) + (p*sin(angle)) .- volt
+
+    for (i, val) in enumerate(arg)
+        if abs(val*kc) < turn
+            arg[i] = K(arg[i], kc)
+        else
+            arg[i] = Kor(arg[i], kc)
+        end
+    end
+    return arg
+end
+
+function wigner(args)
+    iq, ip, q, p, m, angles, volt, kc = args
+    int_s = zeros(Float64, length(angles))
+    for (i, angle) in enumerate(angles)
+        int_s[i] = trapz(volt, vec(m[i, :] .* Kcomp(q, p, angle, volt, kc)))
+    end
+    int_phi = trapz(angles, int_s)
+    return iq, ip, int_phi/(2*pi^2)
+    # return angles, temp
+end
+
+function tomography(m, angles, volt; q1=-2, q2=2, p1=-2, p2=2, density=50, kc = 5)
+    que = []
+    Q = range(q1, q2, length = density)
+    P = range(p1, p2, length = density)
+    W = zeros(Float64, (density, density))
+
+    for q in 1:density
+        for p in 1:density
+            push!(que, (q, p, Q[q], P[p], m, angles, volt, kc))
+        end
+    end
+    results = [wigner(var) for var in que]
+
+    for (iq, ip, w) in results
+        W[iq, ip] = w
+    end
+    return collect(Q), collect(P), Matrix(W)
+end
+
+end
+
